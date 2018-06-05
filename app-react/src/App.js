00299ede0ba5098392cf00b8049e321fbde5e30f
Mathieu Leger-Dalcourt <Mathieu.Leger-Dalcourt@fujitsu.com>
2018-06-05 09:04:48 -0400

    animate panning

--- app-react/src/App.js
+++ app-react/src/App.js
@@ -5,24 +5,37 @@ import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
 export default class App extends Component {
   constructor(props) {
     super(props);
+    this.fujitsuPos = { lat: 46.830545, lng: -71.306222 };
     this.state = {
       position: {
         lat: 46.830545,
         lng: -71.306222
       },
       zoom: 14,
-
+      animate: true,
     };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(e) {
+    this.setState({
+      position: e.latlng,
+    });
   }
 
   render() {
     return (
-      <Map center={this.state.position} zoom={this.state.zoom}>
+      <Map
+        animate={this.state.animate}
+        length={4}
+        onClick={this.handleClick}
+        center={this.state.position}
+        zoom={this.state.zoom}>
         <TileLayer
           attribution='&copy; Fujitsu'
           url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
         />
-        <Marker position={this.state.position}>
+        <Marker position={this.fujitsuPos}>
           <Popup>
             <span>
               Nos bureaux à Québec
